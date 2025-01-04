d03be26ebd7da913631e6490df9acbd29460fbe1
Riza.k.k <153474473+KabdollaRiza@users.noreply.github.com>
2025-01-05 02:46:16 +0500

    Update transactions.py

--- transactions.py
+++ transactions.py
@@ -1,1 +1,15 @@
+from digital_signature import verify
 
+class Transaction:
+    def __init__(self, sender, receiver, amount, signature):
+        self.sender = sender
+        self.receiver = receiver
+        self.amount = amount
+        self.signature = signature
+
+def verify_transaction(transaction, public_key):
+    document = f"{transaction.sender}:{transaction.receiver}:{transaction.amount}"
+    print("Verifying transaction:", document)
+    if not verify(public_key, document, transaction.signature):
+        raise ValueError("Signature is wrong")
+    print("Transaction is valid")
