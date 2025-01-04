1ea0e0363a0bad726ac327fcb20a31424bc5e303
NurasylKh <153474385+NurasylKh@users.noreply.github.com>
2025-01-05 02:47:56 +0500

    Implemented a digital signature mechanism

--- digital_signature.py
+++ digital_signature.py
@@ -1,1 +1,11 @@
+from rsa import encrypt, decrypt
+
+def sign(private_key, document):
+    print("Signing document:", document)
+    return encrypt(private_key, document)
+
+def verify(public_key, document, signature):
+    decrypted_signature = decrypt(public_key, signature)
+    print("Verifying signature. Decrypted:", decrypted_signature)
+    return decrypted_signature == document
 
