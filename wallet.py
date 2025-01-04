decfcdcf2201b91cebea589fbb67b4b106fe246a
Riza.k.k <153474473+KabdollaRiza@users.noreply.github.com>
2025-01-05 02:46:52 +0500

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -1,1 +1,14 @@
+from transaction import Transaction
+from digital_signature import sign
+
+class Wallet:
+    def __init__(self, private_key, public_key):
+        self.private_key = private_key
+        self.public_key = public_key
+
+    def create_transaction(self, receiver, amount):
+        document = f"{self.public_key}:{receiver}:{amount}"
+        print("Creating transaction:", document)
+        signature = sign(self.private_key, document)
+        return Transaction(self.public_key, receiver, amount, signature)
 
