52401096006cb668e448b02e58f39d77fe3249ef
NurasylKh <153474385+NurasylKh@users.noreply.github.com>
2025-01-05 02:46:37 +0500

    Implemented RSA encryption

--- rsa.py
+++ rsa.py
@@ -1,1 +1,47 @@
+import random
+
+def mod_exp(base, exp, mod):
+    result = 1
+    while exp > 0:
+        if exp % 2 == 1:
+            result = (result * base) % mod
+        base = (base * base) % mod
+        exp //= 2
+    return result
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def mod_inverse(e, phi):
+    d, x1, x2, y1, temp_phi = 0, 0, 1, 1, phi
+    while e > 0:
+        temp1, temp2 = temp_phi // e, temp_phi - (temp_phi // e) * e
+        temp_phi, e = e, temp2
+        x, y = x2 - temp1 * x1, d - temp1 * y1
+        x2, x1, d, y1 = x1, x, y1, y
+    if temp_phi == 1:
+        return d + phi
+
+def generate_keys():
+    p = 61  
+    q = 53
+    n = p * q
+    phi = (p - 1) * (q - 1)
+
+    e = random.randrange(1, phi)
+    while gcd(e, phi) != 1:
+        e = random.randrange(1, phi)
+
+    d = mod_inverse(e, phi)
+    return ((e, n), (d, n))  
+
+def encrypt(public_key, plaintext):
+    e, n = public_key
+    return [mod_exp(ord(char), e, n) for char in plaintext]
+
+def decrypt(private_key, ciphertext):
+    d, n = private_key
+    return ''.join([chr(mod_exp(char, d, n)) for char in ciphertext])
 
